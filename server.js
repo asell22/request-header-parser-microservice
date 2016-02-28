f65a37d1783e70824a129a472530c73625adf73f
asell22 <alexanderjsell@gmail.com>
2016-02-28 19:48:29 +0000

    send back json after using regex

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    console.log(req.header('x-forwarded-for'));
-    console.log(req.header('accept-language'));
-    console.log(req.header('user-agent'));
-    res.send();
+    var ipaddress = req.header('x-forwarded-for');
+    
+    var language = req.header('accept-language');
+    language = /^[^,]*/.exec(language)[0];
+    
+    var software = req.header('user-agent');
+    software = software.match(/\((.+?)\)/)[1];
+    
+    res.send({
+        ipaddress: ipaddress,
+        language: language,
+        software: software
+    });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
